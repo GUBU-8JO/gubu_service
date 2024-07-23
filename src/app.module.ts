552fac9adc3685db5003566ec224ad654ec70410
JINSEO KIM <jinseokim@JINSEOui-MacBookPro.local>
2024-07-23 11:09:53 +0900

    Feat: RDS 연결설정 및 연결확인

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -10,8 +13,37 @@ import { SubscriptionHistoriesModule } from './subscription-histories/subscripti
 import { ServicesModule } from './services/services.module';
 import { CategoriesModule } from './categories/categories.module';
 
+const typeOrmModuleOptions = {
+  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: Number(configService.get('DB_PORT')),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: configService.get('DB_SYNC'),
+    logging: true,
+  }),
+  inject: [ConfigService],
+};
+
 @Module({
-  imports: [UsersModule, AuthModule, ReviewsModule, NotificationsModule, UserSubscriptionsModule, SubscriptionHistoriesModule, ServicesModule, CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    UsersModule,
+    AuthModule,
+    ReviewsModule,
+    NotificationsModule,
+    UserSubscriptionsModule,
+    SubscriptionHistoriesModule,
+    ServicesModule,
+    CategoriesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
