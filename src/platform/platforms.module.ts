14bd54674016c6964842a6889f5147e2fdf69d15
gil-hyeon <lgh5498@naver.com>
2024-07-25 18:12:43 +0900

    feat: 플랫폼 전체 가격조회 api, 특정 플랫폼 가격조회 api 생성

--- src/platform/platforms.module.ts
+++ src/platform/platforms.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlatformsService } from './platforms.service';
 import { PlatformsController } from './platforms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Platforms } from './entities/platforms.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Platforms])],
   controllers: [PlatformsController],
   providers: [PlatformsService],
 })
--- src/platform/platforms.service.ts
+++ src/platform/platforms.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateServiceDto } from './dto/create-service.dto';
-import { UpdateServiceDto } from './dto/update-service.dto';
+import { Platforms } from './entities/platforms.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PlatformsService {
-  create(createServiceDto: CreateServiceDto) {
-    return 'This action adds a new service';
+  constructor(
+    @InjectRepository(Platforms)
+    private platformRepositoty: Repository<Platforms>,
+  ) {}
+
+  async findById(id: number): Promise<Platforms> {
+    return await this.platformRepositoty.findOne({
+      where: { id },
+      select: ['id', 'title', 'price'],
+    });
   }
 
-  findAll() {
-    return `This action returns all services`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
-  }
-
-  update(id: number, updateServiceDto: UpdateServiceDto) {
-    return `This action updates a #${id} service`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} service`;
+  async findMany() {
+    return await this.platformRepositoty.find({
+      select: ['id', 'title', 'price'],
+    });
   }
 }
