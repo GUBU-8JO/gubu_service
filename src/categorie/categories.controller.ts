8d052836df730611a938e4c46695400c90ad143f
JINSEO KIM <jinseokim@JINSEOui-MacBookPro.local>
2024-07-25 23:34:07 +0900

    feat: 카테고리 목록 조회 하기 구현 #34

--- src/categorie/categories.controller.ts
+++ src/categorie/categories.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {Controller, Get, HttpStatus, Param, Query} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import {ApiQuery, ApiTags} from "@nestjs/swagger";
 // import { CreateCategoryDto } from './dto/create-category.dto';
 // import { UpdateCategoryDto } from './dto/update-category.dto';
 
+@ApiTags('카테고리 API')
 @Controller('categorie')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return { message: '카테고리 조회에 성공했습니다.' };
+  @Get()
+  @ApiQuery({ name: 'sortField', required: false, type: String, description: '정렬기준을 선택해주세요 ( id or category, 기본값은 id 입니다 )' })
+  @ApiQuery({ name: 'sortOrder', required: false, type: String, description: '오름차순 / 내림차순을 선택해주세요 ( ASC or DESC, 기본값은 ASC 입니다 )' })
+  async findAll(@Query('sortField') sortField: string, @Query('sortOrder') sortOrder: string) {
+    const categories = await this.categoriesService.findAll(sortField, sortOrder);
+    return {
+      status: HttpStatus.OK,
+      message: '카테고리 목록 조회에 성공했습니다.',
+      data: categories,
+    };
   }
 }
