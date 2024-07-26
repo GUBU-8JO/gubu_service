baa95529ab6b87190107a03a18a98810f600dbc6
GinGer-JSK <jskim5701@gmail.com>
2024-07-26 14:58:56 +0900

    fix: 엔티티 수정 및 DB 정리 #39

--- src/review/dto/find-review.dto.ts
+++ src/review/dto/find-review.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsNumber } from "class-validator";
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class FindReviewDto {
   @IsNumber()
   @IsNotEmpty()
-  serviceId: number;
+  platformId: number;
 }
