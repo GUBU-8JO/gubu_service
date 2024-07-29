c0fa4bb0845c13222ebd3aed63e93418004d7454
jeonsuwon <swjeon0330@gmail.com>
2024-07-29 15:56:21 +0900

    feature:create_review

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,1 +1,4 @@
-export class CreateReviewDto {}
+import { PickType } from '@nestjs/swagger';
+import { Reviews } from '../entities/review.entity';
+
+export class CreateReviewDto extends PickType(Reviews, ['rate', 'comment']) {}
--- src/review/reviews.service.ts
+++ src/review/reviews.service.ts
@@ -1,5 +1,28 @@
 import { Injectable } from '@nestjs/common';
-
+import { Reviews } from './entities/review.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateReviewDto } from './dto/create-review.dto';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(
+    @InjectRepository(Reviews)
+    private readonly reviewsRepository: Repository<Reviews>,
+  ) {}
+
+  async create(
+    userId: number,
+    platformId: number,
+    { rate, comment }: CreateReviewDto,
+  ) {
+    const review = this.reviewsRepository.save({
+      userId,
+      platformId,
+      rate,
+      comment,
+    });
+
+    return review;
+  }
+}
