bf30fd2d441dc3cb3665754d4fcfcb4a26dfbbcb
GinGer-JSK <jskim5701@gmail.com>
2024-08-04 22:43:39 +0900

    fix:플랫폼 조회기능 수정 플랫폼 전체 목록 조회 > image url / rating 출력

--- src/platform/platforms.service.ts
+++ src/platform/platforms.service.ts
@@ -13,10 +13,17 @@ export class PlatformsService {
 
   async findAllPlatforms(): Promise<PlatformVo[]> {
     const platforms = await this.platformRepositoty.find({
-      select: ['id', 'title', 'price'],
+      select: ['id', 'title', 'price', 'rating', 'image'],
     });
     return platforms.map(
-      (platform) => new PlatformVo(platform.id, platform.title, platform.price),
+      (platform) =>
+        new PlatformVo(
+          platform.id,
+          platform.title,
+          platform.price,
+          platform.rating,
+          platform.image,
+        ),
     );
   }
 
@@ -41,14 +48,20 @@ export class PlatformsService {
   async findOnePlatformById(id: number): Promise<PlatformVo> {
     const platform = await this.platformRepositoty.findOne({
       where: { id },
-      select: ['id', 'title', 'price'],
+      select: ['id', 'title', 'price', 'rating', 'image'],
     });
     if (!platform) {
       throw new NotFoundException({
         message: '해당 플랫폼이 존재하지 않습니다.',
       });
     }
-    return new PlatformVo(platform.id, platform.title, platform.price);
+    return new PlatformVo(
+      platform.id,
+      platform.title,
+      platform.price,
+      platform.rating,
+      platform.image,
+    );
   }
 }
 
