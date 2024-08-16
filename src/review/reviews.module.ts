635eccac8d8bc5c0b240b27a35b53639c0617f53
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-08-16 15:11:08 +0900

    fix 불필요 코드 제거

--- src/review/reviews.module.ts
+++ src/review/reviews.module.ts
@@ -9,7 +9,7 @@ import {
   CacheModule as NestCacheModule,
   CacheModuleOptions,
 } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-ioredis';
+
 @Module({
   // imports: [
   //   TypeOrmModule.forFeature([Review, Platform, UserSubscription]),
@@ -17,13 +17,7 @@ import * as redisStore from 'cache-manager-ioredis';
   // ],
   imports: [
     TypeOrmModule.forFeature([Review, Platform, UserSubscription]),
-    NestCacheModule.register<CacheModuleOptions>({
-      store: redisStore,
-      host: 'redis-17544.c340.ap-northeast-2-1.ec2.redns.redis-cloud.com',
-      port: 17544,
-      password: 'xE9kzc66rIPGRmdoiIQ7qTNwpN9eM37k',
-      ttl: 600,
-    }),
+    NestCacheModule.register<CacheModuleOptions>({}),
   ],
   controllers: [ReviewsController],
   providers: [ReviewsService],
